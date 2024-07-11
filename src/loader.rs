d555fc9b459735ae9419393c51352f968baf0a51
Martin van der Werff <github@newinnovations.nl>
2024-07-11 17:50:45 +0200

    wip: also use image.rs as extra loader for zip and rar

--- src/loader.rs
+++ src/loader.rs
@@ -3,10 +3,8 @@ use eog::Image;
 use gdk_pixbuf::Pixbuf;
 use gio::{prelude::FileExt, Cancellable, File, MemoryInputStream};
 use glib::{Bytes, ObjectExt};
-use image::{
-    codecs::gif::GifDecoder, io::Reader, AnimationDecoder, GenericImageView, ImageDecoder,
-};
-use std::{fs, path::Path};
+use image::{io::Reader, DynamicImage, GenericImageView};
+use std::{fs, io::Cursor, path::Path};
 
 pub struct Loader {}
 
@@ -22,7 +20,10 @@ impl Loader {
     }
 
     pub fn image_from_memory(buf: Vec<u8>) -> Image {
-        match Self::image_from_memory_gtk(buf) {
+        if let Ok(im) = Self::image_from_memory_gtk(&buf) {
+            return im;
+        }
+        match Self::image_from_memory_image_rs(&buf) {
             Ok(im) => im,
             Err(e) => draw(&format!("Error: {:?}", e)).unwrap(),
         }
@@ -61,101 +62,63 @@ impl Loader {
         Ok(image)
     }
 
-    pub fn image_from_memory_gtk(buf: Vec<u8>) -> MviewResult<Image> {
-        let bytes = Bytes::from_owned(buf);
+    pub fn image_from_memory_gtk(buf: &Vec<u8>) -> MviewResult<Image> {
+        let bytes = Bytes::from(buf);
         let stream = MemoryInputStream::from_bytes(&bytes);
         let image = Image::new_stream(&stream)?;
         Ok(image)
     }
 
+    pub fn image_from_memory_image_rs(buf: &Vec<u8>) -> MviewResult<Image> {
+        let reader = Reader::new(Cursor::new(buf));
+        let reader = reader.with_guessed_format()?;
+        let im = reader.decode()?;
+        Self::image_rs_to_eog_image(im)
+    }
+
     pub fn image_from_file_image_rs(filename: &str) -> MviewResult<Image> {
-        println!("image2");
+        let reader = Reader::open(filename)?;
+        let reader = reader.with_guessed_format()?;
+        let im = reader.decode()?;
+        Self::image_rs_to_eog_image(im)
+    }
 
-        let x = Reader::open(filename)?;
-        let x = x.with_guessed_format()?;
-
-        if x.format() == Some(image::ImageFormat::Gif) {
-            println!("Is gif");
-            // let dec =x.into_decoder().unwrap();
-            let dec = GifDecoder::new(x.into_inner())?;
-            println!("Dimensions: {:?}", dec.dimensions());
-            let x = dec.into_frames();
-
-            // println!("LEN {}", x.count());
-
-            for frame in x {
-                println!("FRAME");
-                let x = frame?;
-                println!("Delay {:?}", x.delay());
-                let _y = x.into_buffer();
-            }
-
-            return Ok(draw("gif").unwrap());
-        }
-
-        // let d = x.unwrap().into_decoder().unwrap();
-
-        // match image::open(Path::new(&filename)) {
-
-        let im = x.decode()?;
-
-        // The dimensions method returns the images width and height
-        println!(
-            "Dynamic image: dimensions={:?}, colortype={:?}",
-            im.dimensions(),
-            im.color()
-        );
+    pub fn image_rs_to_eog_image(im: DynamicImage) -> MviewResult<Image> {
         let (width, height) = im.dimensions();
-        let b = im.as_bytes();
+        let colorspace;
+        let has_alpha;
+        let bits_per_sample;
+        let rowstride;
         match im {
             image::DynamicImage::ImageRgb8(_) => {
-                let data = Bytes::from(b);
-                let colorspace = gdk_pixbuf::Colorspace::Rgb;
-                let has_alpha = false;
-                let bits_per_sample = 8;
-                let rowstride = 3 * width;
-                let pixbuf = Pixbuf::from_bytes(
-                    &data,
-                    colorspace,
-                    has_alpha,
-                    bits_per_sample,
-                    width as i32,
-                    height as i32,
-                    rowstride as i32,
-                );
-                println!("ImageRgb8 {}", pixbuf.width());
-                Ok(Image::new_pixbuf(&pixbuf))
+                colorspace = gdk_pixbuf::Colorspace::Rgb;
+                has_alpha = false;
+                bits_per_sample = 8;
+                rowstride = 3 * width;
             }
             image::DynamicImage::ImageRgba8(_) => {
-                let data = Bytes::from(b);
-                let colorspace = gdk_pixbuf::Colorspace::Rgb;
-                let has_alpha = true;
-                let bits_per_sample = 8;
-                let rowstride = 4 * width;
-                let pixbuf = Pixbuf::from_bytes(
-                    &data,
-                    colorspace,
-                    has_alpha,
-                    bits_per_sample,
-                    width as i32,
-                    height as i32,
-                    rowstride as i32,
-                );
-                println!("ImageRgb8a {}", pixbuf.width());
-                Ok(Image::new_pixbuf(&pixbuf))
+                colorspace = gdk_pixbuf::Colorspace::Rgb;
+                has_alpha = true;
+                bits_per_sample = 8;
+                rowstride = 4 * width;
             }
-            // image::DynamicImage::ImageLuma8(_) => todo!(),
-            // image::DynamicImage::ImageLumaA8(_) => todo!(),
-            // image::DynamicImage::ImageLuma16(_) => todo!(),
-            // image::DynamicImage::ImageLumaA16(_) => todo!(),
-            // image::DynamicImage::ImageRgb16(_) => todo!(),
-            // image::DynamicImage::ImageRgba16(_) => todo!(),
-            // image::DynamicImage::ImageRgb32F(_) => todo!(),
-            // image::DynamicImage::ImageRgba32F(_) => todo!(),
             _ => {
-                println!("Strange stuff");
-                Ok(draw(&format!("Strange stuff {:?}", im.color())).unwrap())
+                return Ok(draw(&format!("Unsupported color space {:?}", im.color())).unwrap());
             }
         }
+        println!(
+            "Image.rs {:?} {width}x{height} alpha={has_alpha}",
+            im.color()
+        );
+        let pixbuf = Pixbuf::from_bytes(
+            &Bytes::from(im.as_bytes()),
+            colorspace,
+            has_alpha,
+            bits_per_sample,
+            width as i32,
+            height as i32,
+            rowstride as i32,
+        );
+        Ok(Image::new_pixbuf(&pixbuf))
     }
 }
