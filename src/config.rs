3f07141869b64d1e05c9e19438c105c417860131
Martin van der Werff <martin.vanderwerff@tno.nl>
2024-08-07 17:22:17 +0200

    feature: auto create default config file

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,61 @@
-use std::{env, fs, io, sync::OnceLock};
+use std::{env, fs::{self, File}, io::{self, BufWriter, Write}, sync::OnceLock};
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Bookmark {
     pub name: String,
     pub folder: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     pub bookmarks: Vec<Bookmark>,
 }
 
+impl Config {
+    pub fn home() -> String {
+        env::var("HOME").unwrap_or_default()
+    }
+
+    fn dir() -> String {
+        format!("{}/.config/mview", Self::home())
+    }
+
+    fn filename() -> String {
+        format!("{}/mview6.json", Self::dir())
+    }
+
+    pub fn save(&self) -> std::io::Result<()> {
+        fs::create_dir_all(Self::dir())?;
+        let file = File::create(Self::filename())?;
+        let mut writer = BufWriter::new(file);
+        serde_json::to_writer_pretty(&mut writer, self)?;
+        writer.flush()?;
+        Ok(())
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        let config = Self {
+            bookmarks: vec![
+                Bookmark {
+                    name: "Home folder".to_string(),
+                    folder: Self::home(),
+                },
+            ],
+        };
+        match config.save() {
+            Ok(_) => println!("Saved default configuration to {}", Self::filename()),
+            Err(_) => println!("Failed to save default configuration to {}", Self::filename()),
+        };
+        config
+    }
+}
+
 fn read_config() -> io::Result<Config> {
-    let homedir = env::var("HOME").unwrap_or_default();
-    let file = fs::File::open(format!("{homedir}/.config/mview/mview6.json"))?;
+    let file = fs::File::open(Config::filename())?;
     let config: Config = serde_json::from_reader(file)?;
     println!("deserialized = {:?}", config);
     Ok(config)
